169d033b8143229fefe4b827251eb7d230eaaffd
Rohit Jha <therohitjha@gmail.com>
2021-03-18 15:42:28 +0530

    Updates

---
+++ precache-manifest.8135940e0e366df38ab72ba74790885d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b962d4d957952e17b53cf996ed452a",
+    "url": "/react-budget-tracker/index.html"
+  },
+  {
+    "revision": "1e75cc91fcec5ceed686",
+    "url": "/react-budget-tracker/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "1e75cc91fcec5ceed686",
+    "url": "/react-budget-tracker/static/js/2.cf43f1dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-budget-tracker/static/js/2.cf43f1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba472f3a0e3e44a1645",
+    "url": "/react-budget-tracker/static/js/main.1bc5ad74.chunk.js"
+  },
+  {
+    "revision": "aa92f7a180172bd63735",
+    "url": "/react-budget-tracker/static/js/runtime-main.d65e970a.js"
+  }
+]);
